02d641eb0016cbdf01e5a77d16386b996853c4ec
BalajiCXIO <balajip@cloudxsupport.com>
2022-08-03 16:28:15 +0530

    Dev account added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-module "sandbox" {
+module "production" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
@@ -23,5 +23,33 @@ module "sandbox" {
     group = "prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "production"
+}
+
+module "Development" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "cxio.test.development@gmail.com"
+    AccountName               = "Development"
+    ManagedOrganizationalUnit = "Development"
+    SSOUserEmail              = "cxio.test.development@gmail.com"
+    SSOUserFirstName          = "Development"
+    SSOUserLastName           = "test"
+  }
+
+  account_tags = {
+    "ENV" = "Dev"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "terraform"
+    change_reason       = "AFT test"
+  }
+
+  custom_fields = {
+    group = "Dev"
+  }
+
+  account_customizations_name = "development"
 }
