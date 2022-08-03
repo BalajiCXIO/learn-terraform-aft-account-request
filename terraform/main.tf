c7c74275906bb7ef542bfc58e397e542c7bce99e
BalajiCXIO <balajip@cloudxsupport.com>
2022-08-03 11:24:55 +0530

    production account provision requested

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,26 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "cxio-test-production@gmail.com"
+    AccountName               = "CXIO-Production"
+    ManagedOrganizationalUnit = "Production"
+    SSOUserEmail              = "cxio-test-production@gmail.com"
+    SSOUserFirstName          = "production"
+    SSOUserLastName           = "test"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "ENV" = "prod"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "terraform"
+    change_reason       = "AFT test"
   }
 
   custom_fields = {
-    group = "non-prod"
+    group = "prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = ""
 }
