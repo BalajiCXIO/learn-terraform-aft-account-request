869a12db579b5392c6e5276f55c82fd124d702a0
BalajiCXIO <balajip@cloudxsupport.com>
2022-08-03 17:50:22 +0530

    third OU testing account

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,7 +14,7 @@ variable "accounts" {
       AccountEmail     = "cxio.ou.test1@gmail.com"
       AccountName = "outest1"
       ManagedOrganizationalUnit = "OU-test"
-      SSOUserEmail = "cxio.ou.test@gmail.com"
+      SSOUserEmail = "cxio.ou.test1@gmail.com"
       SSOUserFirstName = "ou"
       SSOUserLastName = "test1"
       account_customizations_name = "ou-test"
@@ -26,6 +26,15 @@ variable "accounts" {
       SSOUserEmail = "cxio.ou.test2@gmail.com"
       SSOUserFirstName = "ou"
       SSOUserLastName = "test2"
+      account_customizations_name = "ou-test"
+    }
+    "third" = {
+      AccountEmail     = "cxio.ou.test3@gmail.com"
+      AccountName = "outest3"
+      ManagedOrganizationalUnit = "OU-test"
+      SSOUserEmail = "cxio.ou.test3@gmail.com"
+      SSOUserFirstName = "ou"
+      SSOUserLastName = "test3"
       account_customizations_name = "ou-test"
     }
   }
